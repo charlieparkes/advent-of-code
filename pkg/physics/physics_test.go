c351a44bf69f0e135aa6911f0afc46b0e03f5cca
Charlie Mathews <charlie@charliemathews.com>
2021-12-19 01:24:18 -0600

    Clean up pkg/physics

--- pkg/physics/physics_test.go
+++ pkg/physics/physics_test.go
@@ -14,14 +14,6 @@ func TestFinalPositionOfConstAccelObject(t *testing.T) {
 		result       float64
 	}{
 		{0, 0, 26, 5.56, 402.0},
-		// Below are test cases from yr2021 day17 example 1
-		// {0, 2, -1, 1, 2.0},
-		// {0, 2, -1, 2, 3.0},
-		// {0, 2, -1, 3, 3.0},
-		// {0, 2, -1, 4, 2.0},
-		// {0, 2, -1, 5, 0},
-		// {0, 2, -1, 6, -3.0},
-		// {0, 2, -1, 7, -7.0},
 	}
 	for i, tc := range testCases {
 		t.Run(fmt.Sprint(i), func(t *testing.T) {
@@ -31,19 +23,7 @@ func TestFinalPositionOfConstAccelObject(t *testing.T) {
 }
 
 func TestInitialVelocityOfAcceleratingObject(t *testing.T) {
-	v, err := InitialVelocityOfConstAccelObject(0, 10, 0, 1)
+	v, err := InitialVelocityOfConstAccelObject(0, 402, 26, 5.56)
 	assert.NoError(t, err)
-	assert.Equal(t, 10.0, v)
-
-	v, err = InitialVelocityOfConstAccelObject(0, 2, 1, 2)
-	assert.NoError(t, err)
-	assert.Equal(t, 0.0, v)
-
-	// v, err = InitialVelocityOfConstAccelObject(0, 2, -1, 1)
-	// assert.NoError(t, err)
-	// assert.Equal(t, 2.0, v)
-
-	// v, err = InitialVelocityOfConstAccelObject(0, -7, -1, 7)
-	// assert.NoError(t, err)
-	// assert.Equal(t, 2.0, v)
+	assert.Equal(t, 0.0, math.Round(v))
 }
